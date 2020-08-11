a07382b1d305bf6d664bde5cd57ee08100fdfef9
v-almonacid <ealmonacid@gmail.com>
2020-08-11 19:35:30 +0200

    remove into_result trait implementation for DeserializeError

--- rust/src/js_result.rs
+++ rust/src/js_result.rs
@@ -1,15 +1,15 @@
-use crate::panic::{Result, ToResult};
-// use wasm_bindgen::JsValue;
-use cardano_serialization_lib::error::{DeserializeError};
-
-// impl<T> ToResult<T> for std::result::Result<T, JsValue> {
+// use crate::panic::{Result, ToResult};
+// // use wasm_bindgen::JsValue;
+// use cardano_serialization_lib::error::{DeserializeError};
+//
+// // impl<T> ToResult<T> for std::result::Result<T, JsValue> {
+// //   fn into_result(self) -> Result<T> {
+// //     self.map_err(|jsval| format!("{:?}", jsval))
+// //   }
+// // }
+//
+// impl<T> ToResult<T> for std::result::Result<T, DeserializeError> {
 //   fn into_result(self) -> Result<T> {
-//     self.map_err(|jsval| format!("{:?}", jsval))
+//     self.map_err(|e| e.to_string())
 //   }
 // }
-
-impl<T> ToResult<T> for std::result::Result<T, DeserializeError> {
-  fn into_result(self) -> Result<T> {
-    self.map_err(|e| e.to_string())
-  }
-}
