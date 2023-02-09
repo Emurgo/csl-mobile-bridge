456d2ed678219a0dc9bc7989e932ace531b9d60d
lisicky <lisicky@inbox.ru>
2023-02-09 15:05:09 +0400

    change way to pass a null value from rust codebase

--- rust/src/ios/bridge_tools/result.rs
+++ rust/src/ios/bridge_tools/result.rs
@@ -5,6 +5,10 @@ pub trait CResult<T> {
   fn response(&self, val: &mut T, error: &mut CharPtr) -> bool;
 }
 
+pub trait CNullableResult<T> {
+  fn response_nullable(&self, val: &mut T, error: &mut CharPtr) -> bool;
+}
+
 impl<T: Copy> CResult<T> for Result<T> {
   fn response(&self, val: &mut T, error: &mut CharPtr) -> bool {
     match self {
@@ -20,8 +24,8 @@ impl<T: Copy> CResult<T> for Result<T> {
   }
 }
 
-impl <T: Clone> CResult<T> for Result<Option<T>> {
-  fn response(&self, val: &mut T, error: &mut CharPtr) -> bool {
+impl<T: Copy> CNullableResult<T> for Result<Option<T>> {
+  fn response_nullable(&self, val: &mut T, error: &mut CharPtr) -> bool {
     match self {
       Err(err) => {
         *error = err.into_cstr();
@@ -30,7 +34,7 @@ impl <T: Clone> CResult<T> for Result<Option<T>> {
       Ok(value) => {
         match value {
           Some(value) => {
-            *val = value.clone();
+            *val = *value;
             true
           }
           None => {
