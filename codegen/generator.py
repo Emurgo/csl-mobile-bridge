98993e9d7144802a973cf9e83cc10f697b204d4c
lisicky <lisicky@inbox.ru>
2023-02-22 02:29:25 +0400

    fix codegen for method detection

--- codegen/generator.py
+++ codegen/generator.py
@@ -5,12 +5,27 @@ def read_doc():
     f = open(r'./cardano_serialization_lib.json')
     return json.load(f)
 
+def get_method_list(json):
+    methods = set()
+    for (k, v) in json['index'].items():
+        if v["kind"] == "impl" and v['crate_id'] == 0:
+            if "inner" not in v:
+                continue
+            if "items" not in v["inner"]:
+                continue
+            for method_id in v["inner"]["items"]:
+                methods.add(method_id)
+    return methods
+
 def extract_public_members(json):
     members = {}
+    methods = get_method_list(json)
     for (k, v) in json['index'].items():
         if v['visibility'] == 'public' and v['crate_id'] == 0:
-            if v["kind"] == "method":
-                members[k] = v
+            if v["kind"] == "method" or v["id"] in methods:
+                new_member = v.copy()
+                new_member["kind"] = "method"
+                members[k] = new_member
                 continue
             if "docs" in v and "wasm_accessible" in str(v["docs"]):
                 if v["kind"] == 'function':
@@ -23,6 +38,7 @@ def extract_public_members(json):
 def main():
     json = read_doc()
     members = extract_public_members(json)
+    members = {k: v for k, v in sorted(members.items(), key=lambda item: item[1]["name"])}
     api = doc_types.Api(members, json)
     with open('../rust/src/android/bridge.rs', 'w') as f:
         f.writelines(api.to_android_rust_str())
