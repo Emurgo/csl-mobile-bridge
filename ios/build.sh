db87008bd3441d6bb287db613d7a2ff393e9dbcb
stackchain <30806844+stackchain@users.noreply.github.com>
2023-04-20 19:03:05 -0400

    chore: removed old library map

--- ios/build.sh
+++ ios/build.sh
@@ -10,14 +10,6 @@ if [ "${HAS_CARGO_IN_PATH}" -ne "0" ]; then
     source $HOME/.cargo/env
 fi
 
-if [[ -n "${DEVELOPER_SDK_DIR:-}" ]] && [[ "$MAC_OS_VERSION" != "11" ]]; then
-  # Assume we're in Xcode, which means we're probably cross-compiling.
-  # In this case, we need to add an extra library search path for build scripts and proc-macros,
-  # which run on the host instead of the target.
-  # (macOS Big Sur does not have linkable libraries in /usr/lib/.)
-  export LIBRARY_PATH="${DEVELOPER_SDK_DIR}/MacOSX.sdk/usr/lib:${LIBRARY_PATH:-}"
-fi
-
 if [ -z "${PODS_TARGET_SRCROOT}" ]; then
     ROOT_DIR="${SRCROOT}/../rust"
 else
