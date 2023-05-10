72b5dd01172b18fe345505f57e0d5e043fb0fe75
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2023-05-10 18:46:20 -0400

    Fix build (#99)

--- ios/build.sh
+++ ios/build.sh
@@ -10,14 +10,6 @@ if [ "${HAS_CARGO_IN_PATH}" -ne "0" ]; then
     source $HOME/.cargo/env
 fi
 
-if [[ -n "${DEVELOPER_SDK_DIR:-}" ]] && [[ "$MAC_OS_VERSION" != "11" ]]; then
-  # Assume we're in Xcode, which means we're probably cross-compiling.
-  # In this case, we need to add an extra library search path for build scripts and proc-macros,
-  # which run on the host instead of the target.
-  # (macOS Big Sur does not have linkable libraries in /usr/lib/.)
-  export LIBRARY_PATH="${DEVELOPER_SDK_DIR}/MacOSX.sdk/usr/lib:${LIBRARY_PATH:-}"
-fi
-
 if [ -z "${PODS_TARGET_SRCROOT}" ]; then
     ROOT_DIR="${SRCROOT}/../rust"
 else
@@ -33,10 +25,14 @@ cd "${ROOT_DIR}"
 if [[ "$TARGET_DEVICE_PLATFORM_NAME" == "iphonesimulator" ]]  && [[ "$MAC_CURRENT_ARCH" == "arm64" ]]; then
   # If we're building for the arm simulator on an M1 Mac, we need to use the x86_64-apple-ios-sim target.
   # Otherwise, lipo will compile for arm64 iphone that can't run on the simulator.
+  ACTUAL_SDK_PATH=$(xcrun --sdk iphonesimulator --show-sdk-path)
+  export LIBRARY_PATH="${ACTUAL_SDK_PATH}/usr/lib:${LIBRARY_PATH:-}"
   cargo lipo --targets="aarch64-apple-ios-sim"
   LIPO_BIN_TARGET_DIR="aarch64-apple-ios-sim"
 else
-  cargo lipo --xcode-integ
+  ACTUAL_SDK_PATH=$(xcrun --sdk iphoneos --show-sdk-path)
+  export LIBRARY_PATH="${ACTUAL_SDK_PATH}/usr/lib:${LIBRARY_PATH:-}"
+  cargo lipo --xcode-intem
 fi
 
 mkdir -p "${CONFIGURATION_BUILD_DIR}"
--- react-native-haskell-shelley.podspec
+++ react-native-haskell-shelley.podspec
@@ -7,16 +7,15 @@ Pod::Spec.new do |s|
   s.version      = package["version"]
   s.summary      = package["description"]
   s.description  = <<-DESC
-                  react-native-haskell-shelley
+                 Cardano serialization lib bridge 
                    DESC
-  s.homepage     = "https://github.com/Emurgo/react-native-haskell-shelley"
-  # brief license entry:
+  s.homepage     = "https://github.com/Emurgo/csl-mobile-bridge"
   s.license      = "MIT"
   # optional - use expanded license entry instead:
   # s.license    = { :type => "MIT", :file => "LICENSE" }
   s.authors      = { "emurgo" => "contact@emurgo.io" }
-  s.platforms    = { :ios => "11.0" }
-  s.source       = { :git => "https://github.com/Emurgo/react-native-haskell-shelley.git", :tag => "#{s.version}" }
+  s.platforms    = { :ios => "12.4" }
+  s.source       = { :git => "https://github.com/Emurgo/csl-mobile-bridge.git", :tag => "#{s.version}" }
 
   s.source_files = "ios/**/*.{h,c,m,swift,sh}"
   s.requires_arc = true
