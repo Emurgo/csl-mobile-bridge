16660cc4e59a999dac42d4f25cf75741aca65f5a
lisicky <lisicky@inbox.ru>
2023-10-17 14:13:48 +0900

    change ios build script

--- ios/build.sh
+++ ios/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 set -e
+set -x
 
 HAS_CARGO_IN_PATH=`which cargo; echo $?`
-MAC_OS_VERSION=`sw_vers -productVersion | cut -d '.' -f 1`
-MAC_CURRENT_ARCH=`uname -m`
 
 if [ "${HAS_CARGO_IN_PATH}" -ne "0" ]; then
     source $HOME/.cargo/env
@@ -16,28 +15,46 @@ else
     ROOT_DIR="${PODS_TARGET_SRCROOT}/rust"
 fi
 
-CONFIG_PATH=$(echo $CONFIGURATION | sed 's/^[^.]*\.//')
-OUTPUT_DIR=`echo "${CONFIG_PATH}" | tr '[:upper:]' '[:lower:]'`
-LIPO_BIN_TARGET_DIR="universal"
+build_path="$HOME/.cargo/bin:/usr/local/bin:/usr/bin:/bin"
 
-cd "${ROOT_DIR}"
-
-if [[ "$TARGET_DEVICE_PLATFORM_NAME" == "iphonesimulator" ]]  && [[ "$MAC_CURRENT_ARCH" == "arm64" ]]; then
-  # If we're building for the arm simulator on an M1 Mac, we need to use the x86_64-apple-ios-sim target.
-  # Otherwise, lipo will compile for arm64 iphone that can't run on the simulator.
-  ACTUAL_SDK_PATH=$(xcrun --sdk iphonesimulator --show-sdk-path)
-  export LIBRARY_PATH="${ACTUAL_SDK_PATH}/usr/lib:${LIBRARY_PATH:-}"
-  cargo lipo --targets="aarch64-apple-ios-sim"
-  LIPO_BIN_TARGET_DIR="aarch64-apple-ios-sim"
+#
+# Figure out the correct Rust target from the ARCHS and PLATFORM.
+# This script expects just one element in ARCHS.
+case "$ARCHS" in
+	"arm64")	rust_arch="aarch64" ;;
+	"x86_64")	rust_arch="x86_64" ;;
+	*)			echo "error: unsupported architecture: $ARCHS" ;;
+esac
+if [[ "$PLATFORM_NAME" == "macosx" ]]; then
+	rust_platform="apple-darwin"
 else
-  ACTUAL_SDK_PATH=$(xcrun --sdk iphoneos --show-sdk-path)
-  export LIBRARY_PATH="${ACTUAL_SDK_PATH}/usr/lib:${LIBRARY_PATH:-}"
-  cargo lipo --xcode-integ
+	rust_platform="apple-ios"
 fi
+if [[ "$PLATFORM_NAME" == "iphonesimulator" ]]; then
+    if [[ "${rust_arch}" == "aarch64" ]]; then
+        rust_abi="-sim"
+    else
+        rust_abi=""
+    fi
+else
+	rust_abi=""
+fi
+rust_target="${rust_arch}-${rust_platform}${rust_abi}"
+#
+# Build library in debug or release
+build_args=(--manifest-path "${ROOT_DIR}/Cargo.toml" --target "${rust_target}")
+if [[ "$CONFIGURATION" == "Release" ]]; then
+	rust_config="release"
+	env PATH="${build_path}" cargo build --release "${build_args[@]}"
+elif [[ "$CONFIGURATION" == "Debug" ]]; then
+	rust_config="debug"
+	env PATH="${build_path}" cargo build "${build_args[@]}"
+else
+    echo "error: Unexpected build configuration: $CONFIGURATION"
+fi
+#
 
-mkdir -p "${CONFIGURATION_BUILD_DIR}"
-
-cp -f "${ROOT_DIR}"/target/"${LIPO_BIN_TARGET_DIR}"/"${OUTPUT_DIR}"/*.a "${CONFIGURATION_BUILD_DIR}"/
+cp -f "${ROOT_DIR}"/target/${rust_target}/${rust_config}/*.a "${CONFIGURATION_BUILD_DIR}"/
 cp -f "${ROOT_DIR}"/include/*.h "${CONFIGURATION_BUILD_DIR}"/
 
 exit 0
